1ddb978f3030e909cf5d09bbce81a515224a3245
WebNaresh <nareshbhosale73@gmail.com>
2022-10-25 20:02:43 +0530

    10/24/2022

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const route = require('./routes/testroute');
+const route2 = require('./routes/msgRoute');
 const error = require('./utils/error');
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 // import routes
 app.use('/route', route);
+app.use('/message', route2);
 app.use(error);
 module.exports = app;
 // npm i express cookie-parser body-parser bcryptjs
