284673f573c5f6211895ac18b46ddf7ec3a82217
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-10-19 13:17:58 +0530

    last is updated

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const validator = require("validator");
 const User = new mongoose.Schema({
   name: {
     type: String,
