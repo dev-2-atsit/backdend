a8301e7f3e309609fc8dbb4a66a423bb353fc985
Naresh Bhosale <nareshbhosale73@gmail.com>
2024-07-01 19:04:28 +0530

    strict query option

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connectDatabase = () => {
-    mongoose
-        .connect(process.env.LOCALHOST)
-        .then((data) => {
-            console.log(`Mongodb connected with server: ${data.connection.host}`);
-        });
+  mongoose.set("strictQuery", false);
+  mongoose.connect(process.env.LOCALHOST).then((data) => {
+    console.log(`Mongodb connected with server: ${data.connection.host}`);
+  });
 };
 module.exports = connectDatabase;
 // npm i mongoose
